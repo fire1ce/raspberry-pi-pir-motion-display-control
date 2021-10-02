e0726cb09dc1ccecea1d21414f8b5fdd6ae5ab31
fire1ce <stas@3os.org>
2021-10-02 16:00:49 +0300

    initial test

--- motion-screen-control.py
+++ motion-screen-control.py
@@ -4,15 +4,15 @@ from gpiozero import MotionSensor
 from signal import pause
 import os
 
+pir = MotionSensor(17)
 
-def motion():
+while True:
+
+    pir.wait_for_motion()
     print("Motion detected!")
 
+    pir.wait_for_no_motion(10)
+    print("no motion for 10 seconds")
 
-motion.wait_for_no_motion(10)
-print("no motion for 10 seconds")
-
-
-MotionSensor(17) = motion.wait_for_motion
 
 pause()
