9a6e29992df29f85a2b45c2139be9d8c53cb2bbd
fire1ce <stas@3os.org>
2021-10-04 01:13:14 +0300

    initial test

--- motion-screen-control.py
+++ motion-screen-control.py
@@ -4,26 +4,32 @@ from threading import Timer
 from subprocess import run
 
 
-pir = MotionSensor(4) ## PIR Sensor on GPIO4 pin 7
+pir = MotionSensor(4)  # PIR Sensor on GPIO4 pin 7
 timer = None
 
-def turnDisplayOff(): ## Turns off the display after timer is ended
-    run(['vcgencmd', 'display_power' , '0'])
+
+def turnDisplayOff():  # Turns off the display after timer is ended
+    run(['vcgencmd', 'display_power', '0'])
     print("Turning the display Off...")
 
+
 def newTimer():
-    global timer
-    timer = Timer(10, turnDisplayOff) ## Number of seconds before turning the display off
+    global timer  # Number of seconds before turning the display off
+    timer = Timer(60, turnDisplayOff)
     timer.start()
 
-def getDisplayStatus(): ## Return True if display is ON, False if display is off
-    vcgencmdDisplayPower = run(['vcgencmd', 'display_power'], capture_output=True, text=True).stdout.strip()
+
+def getDisplayStatus():  # Return True if display is ON, False if display is off
+    vcgencmdDisplayPower = run(
+        ['vcgencmd', 'display_power'], capture_output=True, text=True).stdout.strip()
     if (vcgencmdDisplayPower == "display_power=1"):
         return True
     else:
         return False
 
+
 newTimer()
+
 
 def turnDisplayOn():
     timer.cancel()
@@ -32,7 +38,8 @@ def turnDisplayOn():
         run(['vcgencmd', 'display_power', '1'])
         print("Turning the display On...")
 
-turnDisplayOn() ## Initial state Display ON, turns off when no motion
+
+turnDisplayOn()  # Initial state Display ON, turns off when no motion
 
 while True:
     pir.when_motion = turnDisplayOn
