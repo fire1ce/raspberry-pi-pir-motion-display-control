e111c9bc864f400bb2408b1534192b4175a8d295
magicmirror@3os.re <magicmirror@3os.re>
2021-10-03 20:26:32 +0300

    testing

--- motion-screen-control.py
+++ motion-screen-control.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from gpiozero import MotionSensor
 from signal import pause
 from threading import Timer
@@ -8,27 +6,21 @@ import os
 pir = MotionSensor(17)
 
 
-def screenOff():
-    print("turning the screen off")
-
+def screenoff():
+    print("screenoff")
 
 def newTimer():
     print("newTimer")
     global timer
-    timer = Timer(10.0, screenOff())
-
+    timer = Timer(10.0, screenoff)
+    timer.start()
 
 newTimer()
 
-
 def screenOn():
-    # timer.cancel()
-    # print("timerCancel")
+    timer.cancel()
     newTimer()
     print("Motion, turning the screen on")
 
-
 while True:
     pir.when_motion = screenOn
-
-pause()
