3ce051c49311d4250376aa793e61624e351aa5b3
magicmirror@3os.re <magicmirror@3os.re>
2021-10-03 21:32:22 +0300

    testing

--- motion-screen-control.py
+++ motion-screen-control.py
@@ -1,10 +1,11 @@
 from gpiozero import MotionSensor
 from signal import pause
 from threading import Timer
-import os
+from subprocess import run
 
-pir = MotionSensor(17)
 
+pir = MotionSensor(4)
+timer = None
 
 def screenoff():
     print("screenoff")
@@ -15,12 +16,21 @@ def newTimer():
     timer = Timer(10.0, screenoff)
     timer.start()
 
+def getScreenStatus():
+    vcgencmdDisplayPower = run(['vcgencmd', 'display_power'], capture_output=True, text=True).stdout.strip()
+    if (vcgencmdDisplayPower == "display_power=1"):
+        return True
+    else:
+        return False
+
 newTimer()
 
 def screenOn():
+
     timer.cancel()
     newTimer()
     print("Motion, turning the screen on")
+    print(getScreenStatus())
 
 while True:
     pir.when_motion = screenOn
